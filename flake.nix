2ae89e521cc9c233592bedf1c7db12fe94a71ce2
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-06-01 21:43:32 +0200

    update flake (switch to flake-parts)

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
   description = "vdt";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs@{ self, flake-parts, nixpkgs }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { pkgs, system, ... }:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
       in rec {
-        packages.default = pkgs.llvmPackages_16.stdenv.mkDerivation {
+        packages.default = pkgs.llvmPackages_18.stdenv.mkDerivation {
           name = "vdt";
           src = self;
           hardeningDisable = [ "all" ];
@@ -19,11 +20,12 @@
           nativeBuildInputs = with pkgs; [ cmake ];
         };
 
-        devShells.default = pkgs.llvmPackages_16.stdenv.mkDerivation {
+        devShells.default = pkgs.llvmPackages_18.stdenv.mkDerivation {
           name = "vdt-dev";
           hardeningDisable = [ "all" ];
           impureUseNativeOptimizations = true;
           nativeBuildInputs = with pkgs; [ cmake ];
         };
-      });
+      };
+    };
 }
